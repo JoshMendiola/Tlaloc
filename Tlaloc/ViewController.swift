aa20f0f9f257509d91cd5b2b4e62f503ea00b353
PhilosophyWithJosh <joshmendiola@icloud.com>
2022-01-19 00:14:20 -0600

    Initial Commit

---
+++ Tlaloc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tlaloc
+//
+//  Created by Joshua Mendiola on 1/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

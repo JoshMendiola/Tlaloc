8ab4a373f47aaf4fdf886ddf2f018f608d062742
PhilosophyWithJosh <joshmendiola@icloud.com>
2022-02-17 11:14:58 -0600

    UI fixes, prepping for introduction of assets and app store related features(notifications, inapp purchases, ETC)

--- Tlaloc/View/plantCell.swift
+++ Tlaloc/View/plantCell.swift
@@ -43,15 +43,3 @@ class plantCell: UITableViewCell
     }
 }
 
-//adds functionality needed for Calender commands
-extension Calendar
-{
-    func numberOfDaysBetween(_ from: Date, and to: Date) -> Int {
-        let fromDate = startOfDay(for: from)
-        let toDate = startOfDay(for: to)
-        let numberOfDays = dateComponents([.day], from: fromDate, to: toDate)
-        
-        return numberOfDays.day! + 1 // <1>
-    }
-}
-

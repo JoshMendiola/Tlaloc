0e6a283dc27c3fdc19b09215310d4cf4cff33e9f
PhilosophyWithJosh <joshmendiola@icloud.com>
2022-04-12 14:52:15 -0500

    still making UI changes, calendar now won't repeat duplicate values

--- Tlaloc/Controller/plantListViewController.swift
+++ Tlaloc/Controller/plantListViewController.swift
@@ -202,7 +202,6 @@ extension plantListViewController: UITableViewDelegate, UITableViewDataSource
         }
         else
         {
-            debugPrint("LOADING CELLS")
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "activityCell") as? activityCell
             else {return UITableViewCell()}
             let plantActivity = plantActivityInfo[indexPath.row]
@@ -226,102 +225,109 @@ extension plantListViewController: UITableViewDelegate, UITableViewDataSource
     //this handles the telling of the core data system that the plant has been succesfully watered
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?
     {
-        //grabs the current date
-        let date = Date()
-        let dateComponents = Calendar.current.dateComponents([.day, .month, .year], from: date)
-        let currentDate = Calendar.current.date(from: dateComponents)!
-        selectedDate = currentDate
-        plantNameHolder = self.plants[indexPath.row].plantName!
-        
-        //true equaling the water choice, this calculates the amount of days until the upcoming water date and resets the day counter to the distance to the upcoming date
-        if(tableDecision == 0)
+        if(tableView == self.tableView)
         {
-            plantWasWatered = true
-            let resetAction = UIContextualAction(style: .normal, title: "Water")
-            { [self] (rowAction, view, completion: (Bool) -> Void) in
-                self.plants[indexPath.row].nextWaterDate = Calendar.current.date(byAdding: .day, value: Int(self.plants[indexPath.row].daysBetweenWater), to: currentDate)!
-                completion(true)
-                
-                //deletes the previously planned notification
-                UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [self.plants[indexPath.row].plantID! + "Water"])
-                
-                //plans a new notification with the new nextwater date
-                let waterContent = UNMutableNotificationContent()
-                waterContent.title = self.plants[indexPath.row].plantName! + " needs water !"
-                waterContent.subtitle = "Log in now and water your plant"
-                waterContent.sound = UNNotificationSound.default
-                let futureWaterNotifDate = self.combineDateWithTime(date: self.plants[indexPath.row].nextWaterDate!, time: self.preferredNotifTime)
-                
-                //makes the notification request
-                let waterTrigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.day,.hour,.minute], from: futureWaterNotifDate!), repeats: true)
-                let waterRequest = UNNotificationRequest(identifier: (self.plants[indexPath.row].plantID! + "Water") , content: waterContent, trigger: waterTrigger)
-                UNUserNotificationCenter.current().add(waterRequest)
-                
-                //reloads the table
-                self.fetchCoreDataObjects()
-                tableView.reloadData()
-            }
+            //grabs the current date
+            let date = Date()
+            let dateComponents = Calendar.current.dateComponents([.day, .month, .year], from: date)
+            let currentDate = Calendar.current.date(from: dateComponents)!
+            selectedDate = currentDate
+            plantNameHolder = self.plants[indexPath.row].plantName!
             
-            resetAction.backgroundColor = UIColor.blue
-            //saves the new changes made to the data
-            self.save { (complete) in
-                if complete
-                {
-                    return
-                }
-            }
-            return UISwipeActionsConfiguration(actions: [resetAction])
-        }
-        //does the same in the case that the fertilizer screen is being looked at
-        else
-        {
-            plantWasWatered = false
-            let resetAction = UIContextualAction(style: .normal, title: "Fertilize")
-            { [self] (rowAction, view, completion: (Bool) -> Void) in
-                self.plants[indexPath.row].nextFertilizerDate = Calendar.current.date(byAdding: .day, value: Int(self.plants[indexPath.row].daysBetweenFertilizer), to: currentDate)!
-                completion(true)
-                
-                //deletes the previously planned notification
-                UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [self.plants[indexPath.row].plantID! + "Fertilizer"])
-                
-                //checks if plant needs fertilizer
-                if self.plants[indexPath.row].needsFertilizer
-                {
-                    //schedules next fertilizer notification with the new nextfertilizer date
-                    let fertilizerContent = UNMutableNotificationContent()
-                    fertilizerContent.title = self.plants[indexPath.row].plantName! + " needs fertilizer !"
-                    fertilizerContent.subtitle = "Log in now and fertilize your plant"
-                    fertilizerContent.sound = UNNotificationSound.default
-                    let futureFertilizerNotifDate = self.combineDateWithTime(date: self.plants[indexPath.row].nextFertilizerDate!, time: self.preferredNotifTime)
-                    let fertilizerTrigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.day,.hour,.minute], from: futureFertilizerNotifDate!), repeats: true)
+            //true equaling the water choice, this calculates the amount of days until the upcoming water date and resets the day counter to the distance to the upcoming date
+            if(tableDecision == 0)
+            {
+                plantWasWatered = true
+                let resetAction = UIContextualAction(style: .normal, title: "Water")
+                { [self] (rowAction, view, completion: (Bool) -> Void) in
+                    self.plants[indexPath.row].nextWaterDate = Calendar.current.date(byAdding: .day, value: Int(self.plants[indexPath.row].daysBetweenWater), to: currentDate)!
+                    completion(true)
+                    
+                    //deletes the previously planned notification
+                    UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [self.plants[indexPath.row].plantID! + "Water"])
+                    
+                    //plans a new notification with the new nextwater date
+                    let waterContent = UNMutableNotificationContent()
+                    waterContent.title = self.plants[indexPath.row].plantName! + " needs water !"
+                    waterContent.subtitle = "Log in now and water your plant"
+                    waterContent.sound = UNNotificationSound.default
+                    let futureWaterNotifDate = self.combineDateWithTime(date: self.plants[indexPath.row].nextWaterDate!, time: self.preferredNotifTime)
                     
                     //makes the notification request
-                    let fertilizerRequest = UNNotificationRequest(identifier: (self.plants[indexPath.row].plantID! + "Fertilizer"), content: fertilizerContent, trigger: fertilizerTrigger)
-                    UNUserNotificationCenter.current().add(fertilizerRequest)
+                    let waterTrigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.day,.hour,.minute], from: futureWaterNotifDate!), repeats: true)
+                    let waterRequest = UNNotificationRequest(identifier: (self.plants[indexPath.row].plantID! + "Water") , content: waterContent, trigger: waterTrigger)
+                    UNUserNotificationCenter.current().add(waterRequest)
+                    
+                    //reloads the table
+                    self.fetchCoreDataObjects()
+                    tableView.reloadData()
                 }
-                //reloads the table
-                self.fetchCoreDataObjects()
-                tableView.reloadData()
-            }
-            resetAction.backgroundColor = UIColor.systemBrown
-            
-            //saves the data
-            self.save { (complete) in
-                if complete
-                {
-                    return
+                
+                resetAction.backgroundColor = UIColor.blue
+                //saves the new changes made to the data
+                self.save { (complete) in
+                    if complete
+                    {
+                        return
+                    }
                 }
+                return UISwipeActionsConfiguration(actions: [resetAction])
             }
-            return UISwipeActionsConfiguration(actions: [resetAction])
+            //does the same in the case that the fertilizer screen is being looked at
+            else
+            {
+                plantWasWatered = false
+                let resetAction = UIContextualAction(style: .normal, title: "Fertilize")
+                { [self] (rowAction, view, completion: (Bool) -> Void) in
+                    self.plants[indexPath.row].nextFertilizerDate = Calendar.current.date(byAdding: .day, value: Int(self.plants[indexPath.row].daysBetweenFertilizer), to: currentDate)!
+                    completion(true)
+                    
+                    //deletes the previously planned notification
+                    UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [self.plants[indexPath.row].plantID! + "Fertilizer"])
+                    
+                    //checks if plant needs fertilizer
+                    if self.plants[indexPath.row].needsFertilizer
+                    {
+                        //schedules next fertilizer notification with the new nextfertilizer date
+                        let fertilizerContent = UNMutableNotificationContent()
+                        fertilizerContent.title = self.plants[indexPath.row].plantName! + " needs fertilizer !"
+                        fertilizerContent.subtitle = "Log in now and fertilize your plant"
+                        fertilizerContent.sound = UNNotificationSound.default
+                        let futureFertilizerNotifDate = self.combineDateWithTime(date: self.plants[indexPath.row].nextFertilizerDate!, time: self.preferredNotifTime)
+                        let fertilizerTrigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.day,.hour,.minute], from: futureFertilizerNotifDate!), repeats: true)
+                        
+                        //makes the notification request
+                        let fertilizerRequest = UNNotificationRequest(identifier: (self.plants[indexPath.row].plantID! + "Fertilizer"), content: fertilizerContent, trigger: fertilizerTrigger)
+                        UNUserNotificationCenter.current().add(fertilizerRequest)
+                    }
+                    //reloads the table
+                    self.fetchCoreDataObjects()
+                    tableView.reloadData()
+                }
+                resetAction.backgroundColor = UIColor.systemBrown
+                
+                //saves the data
+                self.save { (complete) in
+                    if complete
+                    {
+                        return
+                    }
+                }
+                return UISwipeActionsConfiguration(actions: [resetAction])
+            }
         }
+        return UISwipeActionsConfiguration()
     }
     
     //this handles the transition to the editing screen
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        guard let plantEditViewController = storyboard?.instantiateViewController(withIdentifier: "plantEditViewController") as? plantEditViewController else {return}
-        plantEditViewController.initdata(dex: indexPath.row)
-        presentDetail(plantEditViewController)
+        if(tableView == self.tableView)
+        {
+            guard let plantEditViewController = storyboard?.instantiateViewController(withIdentifier: "plantEditViewController") as? plantEditViewController else {return}
+            plantEditViewController.initdata(dex: indexPath.row)
+            presentDetail(plantEditViewController)
+        }
     }
 }
 
@@ -361,6 +367,7 @@ extension plantListViewController
         }
     }
     
+    //checks to see if there are any references to this date in the system at all
     func fetchCalenderInfo(dateToCheck: Date) -> Bool
     {
         guard let managedContext = appDelegate?.persistentContainer.viewContext else {return false}
@@ -388,6 +395,7 @@ extension plantListViewController
         return false
     }
     
+    //grabs all of the instances of plant activity on the specific date passed to it
     func fetchPlantActivityInfo(dateToCheck: Date) -> [PlantCalendar]
     {
         guard let managedContext = appDelegate?.persistentContainer.viewContext else {return []}
@@ -402,10 +410,6 @@ extension plantListViewController
         do
         {
             plantCalendarInfo = try managedContext.fetch(fetchRequest)
-            if (plantCalendarInfo != [])
-            {
-                return plantCalendarInfo
-            }
             return plantCalendarInfo
         }
         catch
@@ -433,6 +437,14 @@ extension plantListViewController
         debugPrint(plantNameHolder)
         debugPrint(plantWasWatered)
         
+        let alreadyInCoreData = checkRecordExists(saveDate!, plantName: plantNameHolder, wasWatered: plantWasWatered)
+        
+        if(alreadyInCoreData)
+        {
+            completion(true)
+            return
+        }
+        
         do
         {
             try managedContext.save()
@@ -444,6 +456,35 @@ extension plantListViewController
             debugPrint("Could not save !: \(error.localizedDescription)")
             completion(false)
         }
+    }
+    
+    func checkRecordExists(_ dateToCheck: Date, plantName: String, wasWatered: Bool) -> Bool {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext
+        else { return false}
+        
+        
+        let fetchRequest = NSFetchRequest<PlantCalendar>(entityName: "PlantCalendar")
+        
+        debugPrint(dateToCheck)
+        debugPrint(plantName)
+        debugPrint(wasWatered)
+        
+        let pred = NSPredicate(format: "activeDay == %@ && plantCaredFor == %@ && wasWatered == %@", argumentArray: [dateToCheck,plantName, wasWatered])
+        
+        fetchRequest.predicate = pred
+        var results: [PlantCalendar] = []
+        
+        do
+        {
+            results = try managedContext.fetch(fetchRequest)
+        }
+        catch
+        {
+            print("error executing fetch request: \(error)")
+        }
+
+        return results.count > 0
+
     }
     
     //this handles the calculations of combining both the dates and time, to create one mega date
@@ -480,25 +521,15 @@ extension plantListViewController: UICollectionViewDelegate, UICollectionViewDat
     {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "calendarCell", for: indexPath) as! calendarCell
         cell.dayLabel.text = totalSquares[indexPath.item]
-        debugPrint(totalSquares[indexPath.item])
         if(totalSquares[indexPath.item] != "")
         {
-            debugPrint("this is the selected date")
-            debugPrint(selectedDate)
-            
             //grabs desired month value
             let monthValue = Calendar.current.dateComponents([.month], from: selectedDate).month
-            debugPrint("this is the month value vvvv")
-            debugPrint(monthValue!)
             
             //grabs desired year value
             let yearValue = Calendar.current.dateComponents([.year], from: selectedDate).year
-            debugPrint("this is the year value vvvv")
-            debugPrint(yearValue!)
             
             let newDate = combineDateWithDay(Int(totalSquares[indexPath.item])!,monthValue: monthValue!, yearValue: yearValue!)
-            debugPrint("this is the final result of addition")
-            debugPrint(newDate)
             cell.configureCell(selectedDate: newDate, wasAnActiveDay: fetchCalenderInfo(dateToCheck: newDate))
         }
         else
--- Tlaloc/View/activityCell.swift
+++ Tlaloc/View/activityCell.swift
@@ -9,10 +9,26 @@ import UIKit
 
 class activityCell: UITableViewCell
 {
+    //Iboutlet for the text field of the activity cell
     @IBOutlet weak var plantName: UILabel!
     
+    //this configures the cell using the plantcalendar object passed into it
     func configureCell(plantActivity: PlantCalendar)
     {
+        //sets the standard look of the cells
+        self.layer.cornerRadius = 15.0
+        
+        //checks to determine color of the cell based on if it was watered or fertilized
+        if(plantActivity.wasWatered == true)
+        {
+            self.backgroundColor = UIColor.systemBlue
+        }
+        else
+        {
+            self.backgroundColor = UIColor.systemBrown
+        }
+        
+        //sets the text field based of the plant name passed into it
         plantName.text = plantActivity.plantCaredFor
     }
 
